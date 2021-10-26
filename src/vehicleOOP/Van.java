d436b3b24b0477a694ea8d068df040f4f28a403f
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-10-27 01:48:44 +0530

    Add Van Class

---
+++ src/vehicleOOP/Van.java
@@ -0,0 +1,24 @@
+package vehicleOOP;
+
+public class Van extends Vehicle {
+
+	
+
+	public Van(int wheels, double d, int gears, double e) {
+		super(wheels, d, gears, e);
+		
+	}
+
+	@Override
+	void status() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void vehicleType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
